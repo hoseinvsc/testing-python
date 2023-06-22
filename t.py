22c8198d5b1c0bd7eb8f1e90be1baa6a7dc03ee4
hoseinvsc <hoseinvsc@gmail.com>
2023-06-22 11:41:41 +0330

    new repozitory

--- t.py
+++ t.py
@@ -1,1 +1,26 @@
-print("hello!")
+
+def www(name,sen):
+    print('hello ',name,'   ',sen)
+
+    return 'done'
+# www(name='ali',sen='17')
+
+
+
+
+
+def days(howdays):
+
+    c = howdays*12*365
+    return c
+
+
+
+
+
+k=days(32)
+print('kazem days : ',k)
+
+
+h=days(17)
+print('hossein days : ',h)
