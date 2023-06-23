f03c2b90f4efa127728fcb04ebb5b163e46698dd
hoseinvsc <hoseinvsc@gmail.com>
2023-06-23 20:33:15 +0330

    test 6 added

---
+++ test6.py
@@ -0,0 +1,13 @@
+def evenorodd(number):
+    if number % 2 == 0:
+        return 0
+    else:
+        return tr
+
+number = int(input("enter number:"))
+result = evenorodd(number)
+
+if (result == 0):
+    print(number,"is true!")
+else:
+    print(number,"is false!")
