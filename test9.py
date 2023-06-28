35b3f362b6b5e630c45e0eb37e9a176c4e07dc91
hoseinvsc <hoseinvsc@gmail.com>
2023-06-28 16:33:32 +0330

    disconter!

--- test9.py
+++ test9.py
@@ -1,8 +1,22 @@
-def discounter(price):
-    discount = price * 0.1
-    discounted = price - discount
+#def discounter(price):
+   # discount = price * 0.1
+  #  discounted = price - discount
+ #   return discounted
+
+#price = 100
+#finalprice = discounter(price)
+#print(finalprice)
+
+
+
+def discounter(price, discount):
+    discounted = price - (price * discount)
     return discounted
 
-price = 100
-finalprice = discounter(price)
-print(finalprice)
+g = float(input("type price:"))
+
+x = float(input("discount:"))
+
+i= discounter(g , x/100)
+
+print(i,"is price with discount!")
