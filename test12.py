868a9865cb16efdf4b3fb4f71409777d22a4f0a2
hoseinvsc <hoseinvsc@gmail.com>
2023-07-20 10:32:20 +0330

    jdatetime

--- test12.py
+++ test12.py
@@ -20,17 +20,3 @@ if type(fnumber) == int:
     print("farsi number=", fnumber)
 else:
     print(fnumber)
-
-
-
-
-
-def turki_to_farsi(turkish_word):
-    farsi_words = {
-        "gharpuz": "هندوانه",
-        "su": "آب"}
-    if turkish_word in farsi_words:
-        return farsi_words[turkish_word]
-turki = input("import the turki word:")
-farsi = turki_to_farsi(turki)
-print(farsi)
---
+++ test14.py
@@ -0,0 +1,16 @@
+import datetime
+import jdatetime 
+
+def jtom(s):
+    a = s.split('-')
+    year = int(a[0])
+    month = int(a[1])
+    day = int(a[2])
+    x = jdatetime.date(year,month,day)
+    q = x.togregorian()
+    return q
+
+
+s='1402-4-29'
+v=jtom(s)
+print(v)
