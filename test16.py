6d780e3b20173ce0192908e440139b22b0b4b1ee
hoseinvsc <hoseinvsc@gmail.com>
2023-09-12 12:11:50 +0330

    ...

---
+++ test16.py
@@ -0,0 +1,21 @@
+def balance_founder(file2, balance2):
+    counter = 0
+    for char in file2:
+        counter = counter + 1
+
+        if char == "(":
+            balance2 = balance2 + 1
+        elif char == ")":
+            balance2 = balance2 - 1
+        if balance2 == 12:
+            return counter
+
+# balance2 = balance_founder[2]
+
+# Example usage:
+file = open("problem1/day1.txt", "r")
+read_file = file.read()
+
+# initial_balance = 0
+result = balance_founder(read_file, 12)
+print(result)
