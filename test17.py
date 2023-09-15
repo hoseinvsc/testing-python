07458d08886031c39974f206d9e14ecb998ff944
hoseinvsc <hoseinvsc@gmail.com>
2023-09-15 12:48:50 +0330

    defanging_IPaddress

---
+++ test17.py
@@ -0,0 +1,7 @@
+def defanging_IPaddress(address):
+    defanged_address = address.replace('.', '[.]')
+    return defanged_address
+
+ip_address = "127.0.0.1"
+defanged_ip = defanging_IPaddress(ip_address)
+print(defanged_ip)
