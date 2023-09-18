990671472c8cb9a3ba4caa83ffeb2e6aaad969e9
hoseinvsc <hoseinvsc@gmail.com>
2023-09-18 10:23:08 +0330

    ...

--- test20.py
+++ test20.py
@@ -7,3 +7,17 @@ def calculate_wire_weight(input_str):
 
 result = calculate_wire_weight("16*4")
 print(result, "kg")
+
+
+def calculate_wire_length(input_str):
+    A_str, N_str = input_str.split('*')
+    A = int(A_str)
+    N = int(N_str) 
+    weight = A * N / 1000000 * 8900
+    r = A / 2
+    area = 3.14 * (r ** 2)
+    m = weight / (area * 8900)
+    return m
+
+result = calculate_wire_length("16*4")
+print(result,"Meter")
