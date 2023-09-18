346b842c1e689be76c4619eea208359da0780d0e
hoseinvsc <hoseinvsc@gmail.com>
2023-09-18 09:02:35 +0330

    ...

--- test20.py
+++ test20.py
@@ -1,6 +1,9 @@
-def calculate_wire_weight(A, N):
+def calculate_wire_weight(input_str):
+    A_str, N_str = input_str.split('*')
+    A = int(A_str)
+    N = int(N_str)
     weight = A * N / 1000000 * 8900
     return weight
 
-result = calculate_wire_weight(16, 4)
-print(result,"kg")
+result = calculate_wire_weight("16*4")
+print(result, "kg")
