2271574b763ba45f33ca802a36b17d42a6b12bd4
hoseinvsc <hoseinvsc@gmail.com>
2023-09-20 16:19:58 +0330

    ...

--- test20.py
+++ test20.py
@@ -1,23 +1,23 @@
-def calculate_wire_weight(input_str):
-    A_str, N_str = input_str.split('*')
+# def calculate_wire_weight(input_str):
+#     A_str, N_str = input_str.split('*')
+#     A = int(A_str)
+#     N = int(N_str)
+#     weight = A * N / 1000000 * 8900
+#     return weight
+
+# result = calculate_wire_weight("16*4")
+# print(result, "kg")
+
+def calculate_wire_length(size, weight):
+    A_str, N_str = size.split('*')
     A = int(A_str)
     N = int(N_str)
-    weight = A * N / 1000000 * 8900
-    return weight
-
-result = calculate_wire_weight("16*4")
-print(result, "kg")
-
-
-def calculate_wire_length(input_str):
-    A_str, N_str = input_str.split('*')
-    A = int(A_str)
-    N = int(N_str) 
-    weight = A * N / 1000000 * 8900
     r = A / 2
     area = 3.14 * (r ** 2)
     m = weight / (area * 8900)
-    return m
+    length = m / (A / 1000)
+    
+    return length
 
-result = calculate_wire_length("16*4")
-print(result,"Meter")
+result = calculate_wire_length("16*4", 20)
+print(result,"meter")
