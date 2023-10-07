4267f823d755007556dbae898b6c22733339f5bf
hoseinvsc <hoseinvsc@gmail.com>
2023-10-07 09:37:33 +0330

    ...

--- test21.py
+++
@@ -1,9 +0,0 @@
-with open('old-tsetmc.txt', 'r') as file:
-    data = file.read()
-lines = data.split(';')
-for line in lines:
-    company_data = line.split(',')
-    company_name = company_data[0].split('@')
-    company_symbol = company_data[1].split('@')
-
-    print("نام شرکت:", company_name, "، نماد شرکت:", company_symbol)
